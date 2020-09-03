4a95cae1264c18086e9fae118ea46388a2754acc
Eric Lawrence <eric@squaretone.com>
2020-09-02 20:17:01 -0500

    Form progress

--- src/templates/register-page.js
+++ src/templates/register-page.js
@@ -36,15 +36,16 @@ const Register = ({data}) => {
     studentEmail: 'student@example.com',
     track: 'engineering',
 
-    parentEmailFather: 'father@example.com',
     parentNameFather: 'Father',
+    parentEmailFather: 'father@example.com',
     parentPhoneFather: '1-800-dad',
-    parentInterestFather: 'no',
     
-    parentEmailMother: 'mother@example.com',
     parentNameMother: 'Mother',
+    parentEmailMother: 'mother@example.com',
     parentPhoneMother: '1-800-mom',
-    parentInterestMother: 'no',
+
+    parentInterest: 'no',
+
   });
 
   return  (
@@ -64,6 +65,8 @@ const Register = ({data}) => {
         >
           <input type="hidden" name="form-name" value="register" />
 
+          <h3>Student Information</h3>
+          {/* Student Name */}
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
               <Field 
@@ -71,7 +74,7 @@ const Register = ({data}) => {
                 label="Student Name"
                 id="student_name_input"
                 type="text"
-                name="plan"
+                name="student_name"
                 // autoComplete="name"
                 value={registerDetails.studentName}
                 onChange={(e) => {
@@ -81,6 +84,7 @@ const Register = ({data}) => {
             </div>
           </div>
 
+          {/* Student Gender */}
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
               <Field 
@@ -106,199 +110,227 @@ const Register = ({data}) => {
                   setRegisterDetails({...registerDetails, studentGender: e.target.value});
                 }}
               />
+
+              <p className="description">
+                <em>Why do we ask?</em> The diversity of the students in the program affects what grants
+                we are elegiable to apply for.
+              </p>
             </div>
           </div>
-        </form>
-
-
-        <form className="w-full max-w-2xl mx-auto">
+          
+          {/* Student Email */}
           <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <Field 
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+              <Field
                 required
-                label="Student Name"
-                id="student_name_input"
-                type="text"
-                name="plan"
+                label="Student Email"
+                id="student_email_input"
+                type="email"
+                name="student_email"
                 // autoComplete="name"
-                value={registerDetails.studentName}
+                value={registerDetails.studentEmail}
                 onChange={(e) => {
-                  setRegisterDetails({...registerDetails, studentName: e.target.value});
+                  setRegisterDetails({ ...registerDetails, studentEmail: e.target.value });
+                }}
+              />
+            </div>
+          </div>
+
+          {/* Student School */}
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+              <Field
+                required
+                label="Student School"
+                id="student_school_input"
+                type="text"
+                name="student_school"
+                // autoComplete="name"
+                value={registerDetails.studentSchool}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, studentSchool: e.target.value });
+                }}
+              />
+            </div>
+          </div>
+          
+          {/* Track */}
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+              <Field
+                required
+                label="Engineering"
+                id="track_engineering_input"
+                type="radio"
+                name="track"
+                // autoComplete="name"
+                value={registerDetails.track}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, track: e.target.value });
                 }}
               />
 
+              <Field
+                required
+                label="Programming"
+                id="track_programing_input"
+                type="radio"
+                name="track"
+                // autoComplete="name"
+                value={registerDetails.track}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, track: e.target.value });
+                }}
+              />
 
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                First Name
-              </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-              <p className="text-red-500 text-xs italic">Please fill out this field.</p>
-            </div>
-            <div className="w-full md:w-1/2 px-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                Last Name
-              </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+              <Field
+                required
+                label="Both"
+                id="track_both_input"
+                type="radio"
+                name="track"
+                // autoComplete="name"
+                value={registerDetails.track}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, track: e.target.value });
+                }}
+              />
             </div>
           </div>
+          
+          <h3>Parent Information</h3>
+          <small>Only 1 parent's information is required but feel free to fill out both</small>
+
+          {/* Father Name */}
           <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                Password
-              </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-              <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+              <Field
+                label="Father's Name"
+                type="text"
+                name="father_name"
+                value={registerDetails.parentNameFather}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, parentNameFather: e.target.value });
+                }}
+              />
+            </div>
+
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+              <Field
+                label="Father's Email"
+                type="text"
+                name="father_email"
+                value={registerDetails.parentEmailFather}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, parentEmailFather: e.target.value });
+                }}
+              />
+            </div>
+
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+              <Field
+                label="Father's Phone"
+                type="tel"
+                name="father_name"
+                value={registerDetails.parentPhoneFather}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, parentPhoneFather: e.target.value });
+                }}
+              />
             </div>
           </div>
-          <div className="flex flex-wrap -mx-3 mb-2">
-            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                City
-              </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
+
+        
+          {/* Mothers Name */}
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+              <Field
+                label="Mother's Name"
+                type="text"
+                name="mother_name"
+                value={registerDetails.parentNameMother}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, parentNameMother: e.target.value });
+                }}
+              />
             </div>
-            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                State
-              </label>
-              <div className="relative">
-                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                  <option>New Mexico</option>
-                  <option>Missouri</option>
-                  <option>Texas</option>
-                </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                </div>
+
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+              <Field
+                label="Mother's Email"
+                type="text"
+                name="mother_email"
+                value={registerDetails.parentEmailMother}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, parentEmailMother: e.target.value });
+                }}
+              />
+            </div>
+
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+              <Field
+                label="Mother's Phone"
+                type="tel"
+                name="mother_name"
+                value={registerDetails.parentPhoneMother}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, parentPhoneMother: e.target.value });
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full aaamd:w-1/2 px-3 mb-6 md:mb-0">
+
+              <div>
+                Are you interested in helping?
+                <span className="description">
+                  Let us know if you might be interested in helping throughout the year or at the Expo
+                </span>
               </div>
+
+
+
+              <Field
+                required
+                label="Yes"
+                type="radio"
+                name="parent_interested"
+                value={registerDetails.parentInterest}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, parentInterest: e.target.value });
+                }}
+              />
+
+              <Field
+                required
+                label="No"
+                type="radio"
+                name="parent_interested"
+                value={registerDetails.parentInterest}
+                onChange={(e) => {
+                  setRegisterDetails({ ...registerDetails, parentInterest: e.target.value });
+                }}
+              />
             </div>
-            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                Zip
-              </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210" />
-            </div>
+          </div>
+
+          
+          <div class="flex items-center justify-between">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Register
+            </button>
           </div>
         </form>
 
 
-
-
-        <form className="register-form w-full max-w-2xl mx-auto" action="/thanks" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-          <input type="hidden" name="form-name" value="register" />
-          
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-
-            </div>
-          </div>
-
-          {/* Student Name */}
-          <p>
-            <label>
-              Student Name
-              <input type="text" name="student_name" />
-            </label>   
-          </p>
-
-          {/* Student Gender */}
-          <div>
-            <label htmlFor="gender-male">
-              Male
-              <input type="radio" id="gender-male" name="gender" value="male" />
-            </label>
-
-            <input type="radio" id="gender-female" name="gender" value="female" />
-            <label htmlFor="gender-female">Female</label>
-
-            <span className="description">
-              <em>Why do we ask?</em> 
-              The diversity of the students in the program affects what grants 
-              we are elegiable to apply for.
-            </span>
-          </div>
-
-          {/* Student Email */}
-          <p>
-            <label>
-              Student Email
-              <input type="email" name="student_email" />
-            </label>
-          </p>
-
-          <div>
-            <label>
-              Student School
-              <input type="text" name="student_school" />
-            </label>
-
-            <label>
-              Student Grade
-              <input type="text" name="grade" />
-            </label>
-          </div>
-
-          {/* Track */}
-          <div>
-            <input type="radio" id="track-eng" name="track" value="engineering only" />
-            <label htmlFor="track-eng">Engineering</label>
-
-            <input type="radio" id="track-prog" name="track" value="programming only" />
-            <label htmlFor="track-prog">Programming</label>
-
-            <input type="radio" id="track-both" name="track" value="both engeering and programming" />
-            <label htmlFor="track-both">Both</label>
-          </div>
-
-          {/* Parent Email */}
-          <p>
-            <label>
-              Parent Email
-              <input type="email" name="email" />
-            </label>
-          </p>
-
-          {/* Parent Name */}
-          <p>
-            <label>
-              Parent Name
-            <input type="text" name="name" />
-            </label>
-          </p>
-
-          {/* Phone */}
-          <p>
-            <label>
-              Parent Phone
-              <input type="tel" name="phone" />
-            </label>   
-          </p>
-
-          <div>
-            Are you interested in helping?
-            <input type="radio" id="help-yes" name="help" value="Yes" />
-            <label htmlFor="help-yes">Yes</label>
-
-            <input type="radio" id="help-no" name="help" value="No" />
-            <label htmlFor="help-no">No</label>
-
-            <span className="description">
-              Let us know if you might be interested in helping throughout the year or at the Expo
-            </span>
-          </div>
         
 
 
 
 
-
-          <p>
-            <label>Message<textarea name="message"></textarea></label>
-          </p>
-          <p className="text-align-right">
-            <button className="button" type="submit">Send Message <span className="icon -right"><RiSendPlane2Line/></span></button>
-          </p>
-        </form>
+      
       </div>
 
     </Layout>
