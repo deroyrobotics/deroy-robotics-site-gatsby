06c0db6cde6b81545f35d4b20e6dceeec6d65f49
Eric Lawrence <eric@squaretone.com>
2020-09-03 22:16:42 -0500

    Change up thank you pages

---
+++ src/pages/register/thanks.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link } from "gatsby"
+import SEO from "../../components/seo"
+import Layout from "../../components/layout"
+
+const Thanks = () => (
+  <Layout className="thanks-page">
+    <SEO title="Thank you" />
+    <div
+      className="wrapper"
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1 className="text-3xl mb-8">Thanks for registering...</h1>
+
+      <p className="my-8">
+        We will get back to you shortly.
+      </p>
+
+      <Link
+        to="/"
+        className="bg-gray-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        Return home
+      </Link>
+    </div>
+  </Layout>
+)
+
+export default Thanks
