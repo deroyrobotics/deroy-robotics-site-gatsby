b9ea661ad37e1203dcb9ea3d94c5299e6aa12951
Eric Lawrence <eric@squaretone.com>
2020-09-03 21:53:01 -0500

    Change submit button type

--- src/templates/register-page.js
+++ src/templates/register-page.js
@@ -288,7 +288,7 @@ const Register = ({data}) => {
           </div>
           
           <div className="flex items-center justify-between pt-4">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
               Register
             </button>
           </div>
