076d6d0eba9b3a60b4f86858ab183c88c0c63f22
Eric Lawrence <elawrence@commonsense.org>
2020-09-09 12:21:52 -0500

    Better header sizing

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
 import React from "react"
 
 const Header = ({children}) => (
-  <header className="site-header p-6 flex z-50 relative shadow">
-    {children}
+  <header className="site-header z-50 relative shadow flex flex-1 justify-center">
+    <div className="site-header-inner max-w-5xl p-6 flex flex-1 ">
+      {children}
+    </div>
   </header>
 )
 
--- src/components/logo.js
+++ src/components/logo.js
@@ -2,8 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 let logoImage = '/assets/deroy-robotics-profile.png'
 const Logo = (props) => (
-  <div className="site-logo absolute top-0 left-0 pl-8 h-24">
-    <Link to="/" style={{top: '-2px', height: '6.5rem'}} className="shadow inline-block relative border border-gray-500 w-48 rounded-b bg-gray-100 px-4">
+  <div className="site-logo  relative">
+    <Link to="/" style={{top: '-1.6rem', height: '6.5rem'}} className="absolute shadow inline-block relative border border-gray-500 w-48 rounded-b bg-gray-100 px-4">
       <img src={logoImage} alt={props.title} className="w-48 h-24" />
     </Link>
   </div>
